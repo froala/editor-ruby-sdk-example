adc8e05c81b2bb6b3efe60f13924ad461a386aaf
Gangadhar <gangadhar.k@CELSYS217-BLR.CELSYSWTC.IN>
2023-05-24 17:49:26 +0530

    Modified development.rb and production.rb,deploy-yes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  config.hosts << "/[a-z0-9]+\.com/"
+  config.hosts << "wysiwyg-editor+[a-z0-9\-.]+\.froala-infra\.com"
   
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
