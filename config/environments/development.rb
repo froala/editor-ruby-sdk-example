4ed69858dd0934999982c475bea966ae6eeaf336
Gangadhar <gangadhar.k@CELSYS217-BLR.CELSYSWTC.IN>
2023-05-25 18:49:17 +0530

    Modified development.rb and production.rb,deploy-yes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,6 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-  config.hosts << ".froala-infra.com"
   # Show full error reports.
   config.consider_all_requests_local = true
   # Enable/disable caching. By default caching is disabled.
